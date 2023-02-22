424eed32756b347e9a5dacd108954fec4eed8064
gap2819@gmail.com <gap2819@gmail.com>
2023-02-22 23:56:53 +0300

    Delete unnecessary namespaces

--- TestGoogle/HomePage.cs
+++ TestGoogle/HomePage.cs
@@ -1,10 +1,4 @@
 ﻿using OpenQA.Selenium;
-using Org.BouncyCastle.Asn1.X509;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestGoogle
 {
